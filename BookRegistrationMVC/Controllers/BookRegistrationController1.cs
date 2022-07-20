d04dbf2d6f4822c2c2f694f2e3684e2ac3c4de14
JoeLehmanJr <joejr65@live.com>
2022-07-19 18:32:59 -0700

    Added the to methods to create the views.

--- BookRegistrationMVC/Controllers/BookRegistrationController1.cs
+++ BookRegistrationMVC/Controllers/BookRegistrationController1.cs
@@ -2,11 +2,28 @@
 
 namespace BookRegistrationMVC.Controllers
 {
-    public class BookRegistrationController1 : Controller
+    public class BookRegistrationController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Book_Registration()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Book_Registration(BookRegistration r)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to database
+                // BookRegistration.Add(p);
+
+                // Return a view
+                return RedirectToAction("Index", "Home");
+            }
+
+            // If the Model State is not valid show the user the same web page,
+            // with validation errors
+            return View(r);
         }
     }
 }
