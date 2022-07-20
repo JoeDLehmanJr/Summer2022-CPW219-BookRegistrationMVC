2e24ad31b5ba92a4e7211ee227a68e91f237e416
JoeLehmanJr <joejr65@live.com>
2022-07-19 18:05:30 -0700

    Created the controller bookRegistration.

---
+++ BookRegistrationMVC/Controllers/BookRegistrationController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookRegistrationMVC.Controllers
+{
+    public class BookRegistrationController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
